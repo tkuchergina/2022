bc466f3f2777377fe8e227d407d22788fdd6e83a
tkuchergina <tkuchergina@gmail.com>
2022-08-19 13:22:37 +0300

    public to private

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -10,13 +11,19 @@ public class Calculator {
     private double result;
     private String answer;
 
-    private double calc(){
-        System.out.println("Enter first Value");
-        firstValue = scanner.nextDouble();
-        System.out.println("Enter second Value");
-        secondValue = scanner.nextDouble();
-        System.out.println("Enter operation");
-        operation = scanner.next();
+    private double calc() {
+        try {
+            System.out.println("Enter first Value");
+            firstValue = scanner.nextDouble();
+            System.out.println("Enter second Value");
+            secondValue = scanner.nextDouble();
+            System.out.println("Enter operation");
+            operation = scanner.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Некорректный ввод");
+
+        }
+
         switch (operation) {
             case "+" -> result = firstValue + secondValue;
             case "-" -> result = firstValue - secondValue;
@@ -24,18 +31,25 @@ public class Calculator {
             case "/" -> result = firstValue / secondValue;
             case "^" -> result = Math.pow(firstValue, secondValue);
             case "%" -> result = firstValue % secondValue;
+            default -> System.out.println("Некорректный оператор");
         }
         System.out.println("Result " + result);
         return result;
     }
 
-    public void useCalc(){
+    public void useCalc() {
         double res;
         answer = "y";
         while ("y".equalsIgnoreCase(answer)) {
             res = calc();
+
             System.out.println("Продолжить? (y/n)");
-            answer = scanner.next();
+            try {
+                answer = new Scanner(System.in).next();
+            }
+            catch (NullPointerException e) {
+                System.out.println(e);
+            }
         }
     }
 }
